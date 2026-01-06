e69a654ace52396524c46e17f2a329e420dc28ea
Alex B <alomew@gmail.com>
2026-01-06 18:39:49 +0000

    implement exit

--- src/main.cs
+++ src/main.cs
@@ -7,6 +7,12 @@ class Program
             Console.Write("$ ");
 
             var wantedCommand = Console.ReadLine();
+
+            switch (wantedCommand)
+            {
+                case "exit":
+                    return;
+            }
         
             Console.WriteLine($"{wantedCommand}: command not found");
         }
--- your_program.sh
+++ your_program.sh
@@ -21,4 +21,4 @@ set -e # Exit early if any commands fail
 #
 # - Edit this to change how your program runs locally
 # - Edit .codecrafters/run.sh to change how your program runs remotely
-dotnet run /tmp/codecrafters-build-csharp/codecrafters-shell "$@"
+dotnet run --no-build /tmp/codecrafters-build-csharp/codecrafters-shell "$@"

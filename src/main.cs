b62d20a0bd04d6b75b771747b3e88f4cc3d9f170
Alex B <alomew@gmail.com>
2026-01-06 18:21:31 +0000

    report invalid command

--- src/main.cs
+++ src/main.cs
@@ -3,5 +3,9 @@ class Program
     static void Main()
     {
         Console.Write("$ ");
+
+        var wantedCommand = Console.ReadLine();
+        
+        Console.WriteLine($"{wantedCommand}: command not found");
     }
 }
